85f688f9e5dccdd43e6835d3856d123403c13626
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-03-17 15:07:39 -0700

    Add new display if there is no website

--- js/Doctor.js
+++ js/Doctor.js
@@ -27,6 +27,10 @@ Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get("https://api.betterdoctor.com/2016-03-01/doctors?query=" + medicalIssue + "&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=" + apiKey)
   .then(function(result) {
     for (var i = 0; i < result.data.length; i++) {
+      if (!result.data[i].practices[0].website) {
+        result.data[i].practices[0].website = "No website found."
+      }
+
       doctors[i] = {
         name: result.data[i].profile.first_name +
           " " + result.data[i].profile.last_name,
@@ -34,10 +38,10 @@ Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
           ", " + result.data[i].practices[0].visit_address.city +
           ", " + result.data[i].practices[0].visit_address.state +
           ", " + result.data[i].practices[0].visit_address.zip,
-        phone_number: result.data[i].practices[0].phones.number,
+        phone_number: result.data[i].practices[0].phones[0].number,
         website: result.data[i].practices[0].website
-      }
-    };
+      };
+    }
     displayDoctors(medicalIssue, doctors);
   }).fail(function(result) {
     $(".results").text("Error in retrieving results.");
