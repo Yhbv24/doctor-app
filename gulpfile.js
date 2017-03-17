dc05f1ab9f0e3a32c2e61d07b04d47c5fb59981e
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-03-17 08:26:34 -0700

    Add a few more Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var utlities = require("gulp-util");
+var buildProduction = utilities.env.production;
 
 gulp.task("browserify", ["concatInterface"], function() {
   return browserify({ entries: ["./js/*-interface.js"] })
@@ -14,4 +17,18 @@ gulp.task("concatInterface", function() {
   return gulp.src(["./js/*-interface.js"])
     .pipe(concat("allConcat.js"))
     .pipe(gulp.dest("./tmp"));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("build", function() {
+  if (buildProduction) {
+    gulp.start("minifyScripts")
+  else {
+    gulp.start("jsBrowserify");
+  }
 });
