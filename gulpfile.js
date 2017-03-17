6bda7f239c66989a230e364c078394f0f501ee1b
Ash Laidlaw <ashlaidlaw@Ashs-MacBook-Pro.local>
2017-03-17 08:46:13 -0700

    Add more tasks

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,7 @@ var lin = require("bower-files")({
     }
   }
 });
+var browserSync = require("browser-sync").create;
 
 gulp.task("browserify", ["concatInterface"], function() {
   return browserify({ entries: ["./js/*-interface.js"] })
@@ -72,3 +73,19 @@ gulp.task("bowerCSS", function() {
 });
 
 gulp.task("bower", ["bowerJS", "bowerCSS"]);
+
+gulp.task("serve", function() {
+  browserSync.init({
+    server: {
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(["js/*.js"], ["jsBuild"]);
+  gulp.watch(["bower.json"], ["bowerBuild"]);
+});
+
+gulp.task("jsBuild", ["jsBrowserify", "jshint"], function() {
+  browserSync.reload;
+});
